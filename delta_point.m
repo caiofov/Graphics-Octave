2e7931e866814ba4e4564fd5ff2d29c98ef1c056
caiofov <cfoviana@gmail.com>
2023-04-02 22:38:14 -0300

    delta point

---
+++ delta_point.m
@@ -0,0 +1,4 @@
+function dp = delta_point(point1, point2)
+    dp.x = point2.x - point1.x# horizontal distance between the points
+    dp.y = point2.y - point1.y# vertical distance between the points
+endfunction
